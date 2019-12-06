aceaf9a114b842427041589b18c717f7097d77e3
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-12-06 15:41:16 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,32 +1,12 @@
-
 document.addEventListener("DOMContentLoaded", () => {
-  // initialize taskList class
-  const taskList = new TaskList();
-  //grab all the necessary DOM elements
-
-  //form and relevant input fields
-  const newTaskForm = document.getElementById("create-task-form");
-  const newTaskDescription = document.getElementById("new-task-description");
-  const newTaskPriority = document.getElementById("new-task-priority");
-
-  //ul where new tasks will live on the DOM
-  const taskUl = document.getElementById("tasks");
-
-  const renderApp = () => (taskUl.innerHTML = taskList.renderTasks());
-  //attach event listeners
-
-  newTaskForm.addEventListener("submit", (e) => {
+  // your code here
+  let input = document.getElementById("new-task-description");
+  let submit = document.getElementById("submit");
+  submit.onclick = function (e) {
+    let tasks = document.getElementById("tasks");
+    let li = document.createElement("li")
+    li.innerHTML = input.value;
+    tasks.appendChild(li);
     e.preventDefault();
-    taskList.createNewTask(newTaskDescription.value);
-    // reset form
-    e.target.reset();
-    renderApp();
-  });
-
-  taskUl.addEventListener("click", (e) => {
-    if (e.target.nodeName === "BUTTON") {
-      taskList.deleteTask(e.target.dataset.description);
-      renderApp();
-    }
-  });
+  }
 });
