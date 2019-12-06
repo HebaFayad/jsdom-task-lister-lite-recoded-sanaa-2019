23c679d1289758e16d328f1a31304f10249773ff
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-12-06 15:43:05 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,47 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-  const input = document.getElementById('new-task-description');
-  const form = document.getElementById('create-task-form');
-  const tasks = document.getElementById("tasks");
-  const priority = document.getElementById('priority');
-
-  form.addEventListener('submit',handleSubmition);
-
-  function deleteTask(e){
-    e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+document.body.addEventListener("DOMContentLoaded", () => {
+  function addLi(input) {
+    let uList = document.getElementById('tasks');
+    let element = document.createElement('li');
+    element.innerHTML = input;
+    uList.appendChild(element);
   }
-
-
-  // function sortList(list){
-  //       let coll = [...list]
-  //       if(coll.length > 2){
-  //         coll.sort(function(a, b) {
-  //         let textA = a.getAttribute('data-sort').toLowerCase()
-  //         let textB = b.getAttribute('data-sort').toLowerCase()
-  //         return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-  //       });
-  //       }
-  //   return coll;
-  // }
-
-  function handleSubmition(e){
+  let a=document.body.querySelector("#create-task-form input[type='submit']")
+  a.addEventListener("click", function (e) {
     e.preventDefault();
-    if(input.value != ""){
-      let option = priority.options[priority.selectedIndex];
-      let sortChar = option.getAttribute('data-sort');
-      let li = document.createElement('li');
-      let btn = document.createElement('button');
-      li.textContent = input.value + ' ';
-      li.setAttribute('data-sort',sortChar);
-      li.style.color = priority.value;
-      btn.innerHTML = 'X';
-      btn.addEventListener('click',deleteTask);
-      li.appendChild(btn);
-      tasks.appendChild(li);
-      // let liList = tasks.getElementsByTagName('li');
-      // let sortedList = sortList(liList);
-      // console.log(sortedList);
-      input.value = "";
-    }
-  }
+    let input = document.getElementById('new-task-description').value;
+    addLi(input);
+    input.value = "";
+
+  });
 });
